c85574fb9899a7440592bd7f3c0d510d19a82b1d
Anshul Sharma <anshul@Anshuls-MacBook-Pro.local>
2019-09-11 12:22:00 +0530

    Returning 200(OK) incase the unauthenticated user tries to submit the rating

--- pulse/survey/views.py
+++ pulse/survey/views.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-from rest_framework import status
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,6 +13,7 @@ class SubmitPulseSurveyView(APIView):
     stat_serializer_class = StatisticSerializer
 
     def post(self, request, format=None):
+        status = HTTP_201_CREATED
         data = deepcopy(request.data)
         username = request.user.username
         if username is not '':
@@ -21,10 +22,11 @@ class SubmitPulseSurveyView(APIView):
             if serializer.is_valid():
                 instance = serializer.save()
                 if instance == error_status.INTEGRITY_ERROR or instance == error_status.HAPPINESS_LEVEL_DOES_NOT_EXIST:
-                    return Response({"error_code": instance}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response({"error_code": instance}, status=HTTP_400_BAD_REQUEST)
             else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+                return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        else:
+            status = HTTP_200_OK
         stat_serializer = self.stat_serializer_class(request.user)
-        return Response(stat_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(stat_serializer.data, status=status)
 
