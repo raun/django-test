c1bcb9798d06541fb9e39e002cf58d5e77f09289
Anshul Sharma <anshul@Anshuls-MacBook-Pro.local>
2019-09-12 02:11:11 +0530

    Added endpoint to authenticate and refresh jwt tokens

--- pulse/pulse/urls.py
+++ pulse/pulse/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('survey/', include('survey.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('auth-login/', obtain_jwt_token),
+    path('auth-refresh/', refresh_jwt_token)
 ]
